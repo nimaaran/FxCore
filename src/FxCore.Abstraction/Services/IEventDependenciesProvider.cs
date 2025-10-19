0ccdfb1aff4de5e27c12e8558542d55e91892400
nima aran <aran.nima@outlook.com>
2025-10-19 10:58:56 +0330

    Simplified passing the required dependencies for creating domain event objects.

---
+++ src/FxCore.Abstraction/Services/IEventDependenciesProvider.cs
@@ -0,0 +1,23 @@
+﻿// ┌──────────────────────────────────────────────────────────────────────────────────────────────┐
+// │ALL RIGHTS RESERVED.                                                                          │
+// │THIS FILE IS PART OF FXCORE FRAMEWORK AND DEVELOPED BY NIMA ARAN AND FXCORE CONTRIBUTORS TEAM.│
+// │FOR MORE INFORMATION ABOUT FXCORE, PLEASE VISIT HTTPS://GITHUB.COM/NIMAARAN/FXCORE            │
+// └──────────────────────────────────────────────────────────────────────────────────────────────┘
+
+namespace FxCore.Abstraction.Services;
+
+/// <summary>
+/// Defines a contract for dependencies that are required for creating events.
+/// </summary>
+public interface IEventDependenciesProvider : IDomainService
+{
+    /// <summary>
+    /// Gets the tracking key generator service.
+    /// </summary>
+    ITrackingKeyGenerator TrackingKeyGenerator { get; }
+
+    /// <summary>
+    /// Gets the date time service.
+    /// </summary>
+    IDateTimeService DateTimeService { get; }
+}

77e82535e1b552b1ac86367b9d4a6486d73f3d16
nima aran <aran.nima@outlook.com>
2025-10-21 13:30:41 +0330

    feat: Initialization of the required projects for IAM domain.

---
+++ src/FxCore.Services.IAM.API/Program.cs
@@ -0,0 +1,29 @@
+﻿// ┌──────────────────────────────────────────────────────────────────────────────────────────────┐
+// │ALL RIGHTS RESERVED.                                                                          │
+// │THIS FILE IS PART OF FXCORE FRAMEWORK AND DEVELOPED BY NIMA ARAN AND FXCORE CONTRIBUTORS TEAM.│
+// │FOR MORE INFORMATION ABOUT FXCORE, PLEASE VISIT HTTPS://GITHUB.COM/NIMAARAN/FXCORE            │
+// └──────────────────────────────────────────────────────────────────────────────────────────────┘
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();

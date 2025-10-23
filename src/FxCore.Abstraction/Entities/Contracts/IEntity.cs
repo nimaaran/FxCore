bee73abb458c738cdd62e16a1d3f190638d68ba6
nima aran <aran.nima@outlook.com>
2025-10-24 01:00:06 +0330

    Refactored and reorganized.

---
+++ src/FxCore.Abstraction/Entities/Contracts/IEntity.cs
@@ -0,0 +1,33 @@
+﻿// ┌──────────────────────────────────────────────────────────────────────────────────────────────┐
+// │ALL RIGHTS RESERVED.                                                                          │
+// │THIS FILE IS PART OF FXCORE FRAMEWORK AND DEVELOPED BY NIMA ARAN AND FXCORE CONTRIBUTORS TEAM.│
+// │FOR MORE INFORMATION ABOUT FXCORE, PLEASE VISIT HTTPS://GITHUB.COM/NIMAARAN/FXCORE            │
+// └──────────────────────────────────────────────────────────────────────────────────────────────┘
+
+using FxCore.Abstraction.Common.Models.Contracts;
+
+namespace FxCore.Abstraction.Entities.Contracts;
+
+/// <summary>
+/// Defines a contract for all entity models.
+/// </summary>
+public interface IEntity : IDataModel
+{
+    /// <summary>
+    /// Gets a value indicating whether the object is removed or not.
+    /// </summary>
+    bool Removed { get; }
+}
+
+/// <summary>
+/// Defines a contract for defining all entity models.
+/// </summary>
+/// <typeparam name="TId">Type of the entity id.</typeparam>
+public interface IEntity<TId> : IEntity
+    where TId : notnull
+{
+    /// <summary>
+    /// Gets the entity id.
+    /// </summary>
+    TId Id { get; }
+}
---
+++ src/FxCore.Abstraction/Entities/EntityBase.cs
@@ -0,0 +1,46 @@
+﻿// ┌──────────────────────────────────────────────────────────────────────────────────────────────┐
+// │ALL RIGHTS RESERVED.                                                                          │
+// │THIS FILE IS PART OF FXCORE FRAMEWORK AND DEVELOPED BY NIMA ARAN AND FXCORE CONTRIBUTORS TEAM.│
+// │FOR MORE INFORMATION ABOUT FXCORE, PLEASE VISIT HTTPS://GITHUB.COM/NIMAARAN/FXCORE            │
+// └──────────────────────────────────────────────────────────────────────────────────────────────┘
+
+using FxCore.Abstraction.Common.Models;
+using FxCore.Abstraction.Entities.Contracts;
+
+namespace FxCore.Abstraction.Entities;
+
+/// <summary>
+/// As a base class, it implements the common attributes and behaviors of entity models.
+/// </summary>
+/// <typeparam name="TId">Type of the entity id.</typeparam>
+/// <param name="id">See <see cref="Id"/>.</param>
+/// <param name="removed">See <see cref="Removed"/>.</param>
+public abstract class EntityBase<TId>(TId id, bool removed) : IEntity<TId>
+    where TId : notnull
+{
+    /// <summary>
+    /// Gets the entity id.
+    /// </summary>
+    public TId Id { get; private set; } = id;
+
+    /// <summary>
+    /// Gets a value indicating whether the object is removed or not.
+    /// </summary>
+    public bool Removed { get; private set; } = removed;
+
+    /// <summary>
+    /// Removes the object and set the flag.
+    /// </summary>
+    /// <returns>An object as type of the <see cref="Result"/>.</returns>
+    protected virtual Result Remove()
+    {
+        if (this.Removed)
+        {
+            return Result.Terminated(ResultCodes.NOT_MODIFIED);
+        }
+
+        this.Removed = true;
+
+        return Result.Completed();
+    }
+}

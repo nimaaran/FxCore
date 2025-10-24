580784d85fc3b2cc0b39a64928e7f8a3dd2967ea
nima aran <aran.nima@outlook.com>
2025-10-24 15:31:19 +0330

    feat: created required extension projects for supporting EF/Postgres in the persistence layer.

---
+++ src/FxCore.Extensions.EF/DataContextBase.cs
@@ -0,0 +1,89 @@
+﻿// ┌──────────────────────────────────────────────────────────────────────────────────────────────┐
+// │ALL RIGHTS RESERVED.                                                                          │
+// │THIS FILE IS PART OF FXCORE FRAMEWORK AND DEVELOPED BY NIMA ARAN AND FXCORE CONTRIBUTORS TEAM.│
+// │FOR MORE INFORMATION ABOUT FXCORE, PLEASE VISIT HTTPS://GITHUB.COM/NIMAARAN/FXCORE            │
+// └──────────────────────────────────────────────────────────────────────────────────────────────┘
+
+using FxCore.Abstraction.Common.Models.Contracts;
+using FxCore.Abstraction.Entities.Contracts;
+using FxCore.Abstraction.Persistence.DataContexts.Contracts;
+using Microsoft.EntityFrameworkCore;
+
+namespace FxCore.Extensions.EF;
+
+/// <summary>
+/// As a base class, it defines and implements the required attributes and behaviors of EF-based
+/// data contexts.
+/// </summary>
+public abstract class DataContextBase : DbContext, IDataContext
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="DataContextBase"/> class.
+    /// </summary>
+    public DataContextBase()
+    {
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="DataContextBase"/> class.
+    /// </summary>
+    /// <param name="options"><inheritdoc/></param>
+    public DataContextBase(DbContextOptions options)
+        : base(options)
+    {
+    }
+
+    /// <inheritdoc/>
+    public virtual IReadOnlyCollection<IDataModel> GetTrackedObject()
+    {
+        var list = new List<IDataModel>();
+        var entries = this.ChangeTracker
+           .Entries()
+           .Select(x => x.Entity)
+           .ToList();
+
+        foreach (var entry in entries)
+        {
+            list.Add((IDataModel)entry);
+        }
+
+        return list.AsReadOnly();
+    }
+
+    /// <inheritdoc/>
+    public virtual IQueryable<TModel> GetModel<TModel>()
+        where TModel : class, IDataModel
+    {
+        return this.Set<TModel>().AsQueryable();
+    }
+
+    /// <inheritdoc/>
+    public virtual void Create<TEntity>(TEntity entity)
+        where TEntity : class, IEntity
+    {
+        this.Entry(entity).State = EntityState.Added;
+    }
+
+    /// <inheritdoc/>
+    public virtual void Delete<TEntity>(TEntity entity)
+        where TEntity : class, IEntity
+    {
+        this.Entry(entity).State = EntityState.Deleted;
+    }
+
+    /// <inheritdoc/>
+    public new virtual void Update<TEntity>(TEntity entity)
+        where TEntity : class, IEntity
+    {
+        this.Entry(entity).State = EntityState.Modified;
+    }
+
+    /// <inheritdoc/>
+    public virtual Task<List<TModel>> ReadAsync<TModel>(
+        IQueryable<TModel> query,
+        CancellationToken cancellationToken)
+        where TModel : class, IDataModel
+    {
+        return query.ToListAsync<TModel>(cancellationToken);
+    }
+}
---
+++ src/FxCore.Extensions.EF/QueryBuilder.cs
@@ -0,0 +1,55 @@
+﻿// ┌──────────────────────────────────────────────────────────────────────────────────────────────┐
+// │ALL RIGHTS RESERVED.                                                                          │
+// │THIS FILE IS PART OF FXCORE FRAMEWORK AND DEVELOPED BY NIMA ARAN AND FXCORE CONTRIBUTORS TEAM.│
+// │FOR MORE INFORMATION ABOUT FXCORE, PLEASE VISIT HTTPS://GITHUB.COM/NIMAARAN/FXCORE            │
+// └──────────────────────────────────────────────────────────────────────────────────────────────┘
+
+using FxCore.Abstraction.Common.Models.Contracts;
+using FxCore.Abstraction.Persistence.Paging.Contracts;
+using FxCore.Abstraction.Persistence.Sorting.Contracts;
+using FxCore.Abstraction.Persistence.Specifications.Contracts;
+
+namespace FxCore.Extensions.EF;
+
+/// <summary>
+/// Implements a query builder for EF.
+/// </summary>
+public sealed class QueryBuilder : IQueryBuilder
+{
+    /// <inheritdoc/>
+    public IQueryable<TModel> Build<TModel>(
+        IQueryable<TModel> baseQuery,
+        ISpecification<TModel> specification,
+        IPager<TModel> pager)
+        where TModel : class, IDataModel
+    {
+        IQueryable<TModel> SetSorter(IQueryable<TModel> query, ISorter<TModel> sorter)
+        {
+            if (sorter.Ascending)
+            {
+                query = query.OrderBy(sorter.Column);
+            }
+            else
+            {
+                query = query.OrderByDescending(sorter.Column);
+            }
+
+            if (sorter.Next is not null)
+            {
+                query = SetSorter(query, sorter.Next);
+            }
+
+            return query;
+        }
+
+        var criterion = specification.Criterion!.Export();
+
+        var query = baseQuery.Where(criterion);
+
+        query = SetSorter(query, pager.Sorter);
+        query = query.Skip(pager.PageSize * pager.PageIndex);
+        query = query.Take(pager.PageSize);
+
+        return query;
+    }
+}
